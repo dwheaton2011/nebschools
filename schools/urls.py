ccdbc36a98928b12ba0d160b5a1cc90300c50a9f
Daniel Wheaton <dwheaton2011@gmail.com>
2015-11-30 15:20:48 -0600

    still broken

--- schools/urls.py
+++ schools/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf.urls import *
 from django.contrib import admin
 from nebschools import views
+import nebschools
 
 urlpatterns = [
-    url(r'^admin/', include(urls)),
-    url(r'^$', views.index,  include(urls))
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^schools', include(nebschools.urls))
 ]
